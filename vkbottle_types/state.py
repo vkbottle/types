2c7b7d6e5ae2f6d56b1ecba87da59116b6303554
timoniq <52574917+timoniq@users.noreply.github.com>
2020-11-18 20:59:47 +0300

    add state peer payload [stable]

--- vkbottle_types/state.py
+++ vkbottle_types/state.py
@@ -11,6 +11,7 @@ class BaseStateGroup(IntEnum):
 class StatePeer(BaseModel):
     peer_id: int
     state: int
+    payload: dict
 
     def get_state_path(self) -> Tuple[Type[BaseStateGroup], BaseStateGroup]:
         return self.state.__class__, self.state
