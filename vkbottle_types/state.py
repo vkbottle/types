064f7357fa2919ca8ecfaa92300e642dbdd0109e
FeeeeK <26704473+FeeeeK@users.noreply.github.com>
2022-01-08 03:37:31 +0300

    Another fix for deprecated states

--- vkbottle_types/state.py
+++ vkbottle_types/state.py
@@ -6,9 +6,23 @@ class BaseStateGroup:
             "use vkbottle.BaseStateGroup instead",
         )
 
+    def __init_subclass__(cls) -> None:
+        raise DeprecationWarning(
+            "BaseStateGroup from vkbottle_types is deprecated "
+            "and will be removed in future releases, "
+            "use vkbottle.BaseStateGroup instead",
+        )
+
 
 class StatePeer:
     def __init__(self, *args, **kwargs):
+        raise DeprecationWarning(
+            "StatePeer from vkbottle_types is deprecated "
+            "and will be removed in future releases, "
+            "use vkbottle.StatePeer instead"
+        )
+
+    def __init_subclass__(cls):
         raise DeprecationWarning(
             "StatePeer from vkbottle_types is deprecated "
             "and will be removed in future releases, "
