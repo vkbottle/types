0994d3d5b93adb91051d0c7cf3cef5fa5da40c0a
FeeeeK <26704473+FeeeeK@users.noreply.github.com>
2023-01-30 06:19:04 +0300

    Remove deprecated states

--- vkbottle_types/__init__.py
+++ vkbottle_types/__init__.py
@@ -1,5 +1,4 @@
 from .events import GroupTypes, UserTypes
-from .state import BaseStateGroup, StatePeer
 
 API_URL = "https://api.vk.com/method/"
 API_VERSION = "5.131"
@@ -7,8 +6,6 @@ API_VERSION = "5.131"
 __all__ = (
     "API_URL",
     "API_VERSION",
-    "BaseStateGroup",
     "GroupTypes",
-    "StatePeer",
     "UserTypes",
 )
