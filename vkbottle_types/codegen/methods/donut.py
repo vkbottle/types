44fb5c6d279b2c2efdc0d78af6d579c3b5cae0f3
Georgiy Pliev <howluwqz1@gmail.com>
2024-08-02 16:11:31 +0300

    fix: unused imports

--- vkbottle_types/codegen/methods/donut.py
+++ vkbottle_types/codegen/methods/donut.py
@@ -1,11 +1,15 @@
 import typing
 
 from vkbottle_types.methods.base_category import BaseCategory
-from vkbottle_types.objects import BaseUserGroupFields, DonutDonatorSubscriptionInfo
+from vkbottle_types.objects import BaseUserGroupFields, DonutDonatorSubscriptionInfo, UsersUserFull
 from vkbottle_types.responses.base import (
     BaseBoolResponse,
 )
 from vkbottle_types.responses.donut import *  # noqa: F401,F403
+from vkbottle_types.responses.groups import (
+    GroupsGetMembersFieldsResponse,
+    GroupsGetMembersFieldsResponseModel,
+)
 
 
 class DonutCategory(BaseCategory):
--- vkbottle_types/responses/donut.py
+++ vkbottle_types/responses/donut.py
@@ -1,13 +1,6 @@
 import inspect
-import typing
 
-from vkbottle_types.base_model import BaseModel, Field
 from vkbottle_types.codegen.responses.donut import *  # noqa: F403,F401
-from vkbottle_types.codegen.responses.groups import (
-    GroupsGetMembersFieldsResponse,
-    GroupsGetMembersFieldsResponseModel,
-)
-from vkbottle_types.objects import UsersUserFull
 
 from .base_response import BaseResponse
 
