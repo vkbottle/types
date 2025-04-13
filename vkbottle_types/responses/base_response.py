0818f6011c5e296f25e27f380151aa2620fa307b
luwqz1 <howluwqz1@gmail.com>
2025-04-14 02:46:00 +0300

    fix

--- vkbottle_types/responses/base_response.py
+++ vkbottle_types/responses/base_response.py
@@ -6,15 +6,15 @@ from vkbottle_types.base_model import BaseModel, Field
 
 class BaseResponse(BaseModel):
     response: Any
-    _raw_json: Optional[str] = Field(default=None)
+    raw_json: Optional[str] = Field(default=None)
 
     @property
-    def raw_json(self) -> str:
-        if not self._raw_json:
+    def raw(self) -> str:
+        if not self.raw_json:
             raise AttributeError(
                 "You cannot get raw_json from here. Get a full raw_json from unnested response."
             )
-        return self._raw_json
+        return self.raw_json
 
 
 class DictResponse(BaseResponse):
@@ -24,7 +24,7 @@ class DictResponse(BaseResponse):
         super().__init__(response=data)
 
     @property
-    def _raw_json(self) -> Optional[str]:  # type: ignore
+    def raw(self) -> Optional[str]:  # type: ignore
         if not self.response:
             return None
         return json.dumps(self.response)
