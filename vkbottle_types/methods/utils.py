c6f0837fe0b69967baf9c4c337e5b48ead4b90ef
luwqz1 <howluwqz1@gmail.com>
2025-04-23 20:49:39 +0300

    fixes response type for the 'utils.resolve_screen_name' method

--- vkbottle_types/methods/utils.py
+++ vkbottle_types/methods/utils.py
@@ -1,1 +1,25 @@
-from vkbottle_types.codegen.methods.utils import *  # noqa: F403,F401
+import typing
+
+from vkbottle_types.codegen.methods.utils import *  # noqa: F403,F401  # type: ignore
+from vkbottle_types.objects import UtilsDomainResolved
+from vkbottle_types.responses import UtilsResolveScreenNameResponse  # noqa: F401,F403  # type: ignore
+
+
+class UtilsCategory(UtilsCategory):  # type: ignore
+    async def resolve_screen_name(  # type: ignore
+        self,
+        screen_name: str,
+        **kwargs: typing.Any,
+    ) -> typing.Union[UtilsDomainResolved, typing.List[typing.Any]]:
+        """Method `utils.resolveScreenName()`
+
+        :param screen_name: Screen name of the user, community (e.g., 'apiclub,' 'andrew', or 'rules_of_war'), or application.
+        """
+
+        params = self.get_set_params(locals())
+        response = await self.api.request("utils.resolveScreenName", params)
+        model = UtilsResolveScreenNameResponse
+        return model(**response).response
+
+
+__all__ = ("UtilsCategory",)
