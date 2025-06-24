ab900129b2b7d51234b7587c4b2664b33d1a5b5e
Georgy Pliev <howluwqz1@gmail.com>
2025-06-24 15:37:46 +0300

    fix version check

--- vkbottle_types/base_model.py
+++ vkbottle_types/base_model.py
@@ -92,7 +92,7 @@ class BaseEnumMeta(enum.EnumMeta, type):
                 classdict=classdict,
             )
 
-            if sys.version_info > (3, 10):
+            if sys.version_info >= (3, 11):
                 kwargs.update(
                     dict(
                         boundary=boundary,
